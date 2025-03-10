35bc8514c115eda6ecd39430f5660dd5b8f59f89
Lakshayupadhaya <lakshayupadhaya24@gmail.com>
2025-03-11 00:01:46 +0530

    [Lakshay] Add: Added Role Property to user entity

--- RepositoryLayer/Entity/UserEntity.cs
+++ RepositoryLayer/Entity/UserEntity.cs
@@ -18,6 +18,8 @@ namespace RepositoryLayer.Entity
 
         public string Email { get; set; }
 
-        public string Password { get; set; }    
+        public string Password { get; set; }
+
+        public string Role { get; set; } = "User";
     }
 }
