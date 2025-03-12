fb5dbb7957ad35c472f96985befbc60ebdc95a8d
Lakshayupadhaya <lakshayupadhaya24@gmail.com>
2025-03-12 20:40:27 +0530

    [lakshay] Add: Added SECTION2 UC2 part

--- BusinessLayer/Interface/IAddressBookBL.cs
+++ BusinessLayer/Interface/IAddressBookBL.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ModelLayer.DTO;
+using RepositoryLayer.Entity;
 
 namespace BusinessLayer.Interface
 {
     public interface IAddressBookBL
     {
 
+        List<AddressBookEntity> GetAllContactsBL();
+
+        AddressBookDTO GetContactByIDBL(int id);
+
+        AddressBookDTO UpdateContactByIDBL(int id, AddressBookDTO updateContact);
+
+        CreateContactDTO AddContactBL(AddressBookDTO createContact);
+
+
+        AddressBookDTO DeleteContactByIDBL(int id);
     }
 }
